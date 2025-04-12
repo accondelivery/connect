3f0f45627830894b778ee76174828eb8f5aa82a1
Lincon Dias <lincond@protonmail.com>
2025-04-12 09:25:05 -0300

    refactor: adiciona json schema to ts

--- lib/core/interfaces/integration-meta.interface.ts
+++ lib/core/interfaces/integration-meta.interface.ts
@@ -1,8 +1,10 @@
+import { JSONSchema } from 'json-schema-to-ts';
+
 export interface IntegrationMeta {
-  id: string
-  title: string
-  logoUrl:string
-  websiteUrl: string
-  description: string
-  configSchema: Record<string, any>
+  id: string;
+  title: string;
+  logoUrl: string;
+  websiteUrl: string;
+  description: string;
+  configSchema: JSONSchema;
 }
--- lib/core/interfaces/output-order-integration.interface.ts
+++ lib/core/interfaces/output-order-integration.interface.ts
@@ -1,6 +1,6 @@
 import { IntegrationEvent } from '../types/integration-event.type';
 import { Order } from '../types/order.type';
 
-export interface OutputOrderIntegration {
-  onOrderCreated<C>(order: Order, config: C): Promise<IntegrationEvent[]>;
+export interface OutputOrderIntegration<C> {
+  onOrderCreated(order: Order, config: C): Promise<void>;
 }
