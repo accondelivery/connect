e16519d00b25cebd847ccd31712931210861352c
Lincon Dias <lincond@protonmail.com>
2025-04-15 13:54:12 -0300

    fix: importa FoodyModule no ConnectModule

--- lib/core/connect.module.ts
+++ lib/core/connect.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { IntegrationRegistryService } from './integration-registry.service';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 import { ConnectService } from './connect.service';
+import { FoodyModule } from '../foody/foody.module';
 
 @Module({
-  imports: [EventEmitterModule],
+  imports: [EventEmitterModule, FoodyModule],
   providers: [ConnectService, IntegrationRegistryService],
   exports: [ConnectService],
 })
