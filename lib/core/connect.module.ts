0363305ae7449ad8b3e202810c82c55bfdd6ba33
Lincon Dias <lincond@protonmail.com>
2025-04-15 14:16:54 -0300

    refactor: adiciona HttpModule no AcconConnectModule

--- lib/core/connect.module.ts
+++ lib/core/connect.module.ts
@@ -3,9 +3,10 @@ import { IntegrationRegistryService } from './integration-registry.service';
 import { ConnectService } from './connect.service';
 import { FoodyModule } from '../foody/foody.module';
 import { EventsModule } from './events';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [EventsModule, FoodyModule],
+  imports: [EventsModule, HttpModule, FoodyModule],
   providers: [ConnectService, IntegrationRegistryService],
   exports: [ConnectService],
 })
