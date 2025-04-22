3221d68f4e7b5ded934d162f2e8340321675f171
Lincon Dias <lincond@protonmail.com>
2025-04-22 19:03:49 -0300

    feat(order-output): adiciona eventos onOrderUpdated e onOrderCanceled na interface OrderOutput

--- lib/core/interfaces/order-output-integration.interface.ts
+++ lib/core/interfaces/order-output-integration.interface.ts
@@ -2,4 +2,6 @@ import { Order } from '../types/order.type';
 
 export interface OrderOutputIntegration<C> {
   onOrderCreated(order: Order, config: C): Promise<void>;
+  onOrderUpdated?(order: Order, config: C): Promise<void>;
+  onOrderCanceled?(order: Order, config: C): Promise<void>;
 }
